aaa8f43f36c15806a5c995b7d6cb651decdee107
komal <joshikomal252@gmail.com>
2021-10-02 11:09:10 +0530

    added accent

--- texttospeech.py
+++ texttospeech.py
@@ -6,8 +6,17 @@ file = open(Script)
 text = file.read()
 
 language = 'en'
+accent = input("Enter the accent you want UK/USA/India/Australia:")
+if accent.lower()=='uk':
+    accent='co.uk'
+elif accent.lower()=='usa':
+    accent='com'
+elif accent.lower()=='india':
+    accent='co.in'
+else:
+    accent='com.au'
+obj= gTTS(text=text, lang=language, slow=False,  tld=accent )
 
-obj= gTTS(text=text, lang=language, slow=False, )
 Filename= input("Enter Audio Filename to Save (With Extension): ")
 obj.save(Filename)
 
