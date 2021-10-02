bbc7757f7e057f03918446942ebb68974d938da7
Motasim <makkimotasim@gmail.com>
2021-10-02 18:31:42 +0530

    feat: Added Spanish accent

--- texttospeech.py
+++ texttospeech.py
@@ -6,7 +6,7 @@ file = open(Script)
 text = file.read()
 a= ''
 language = 'en'
-b= int(input("which accent do you want? \n 1) Australian \n 2) American \n 3) British \n 4)Indian \n 5) Canadian \n 6) Irish \n 7) South African \n (Select the number corresponding):"))
+b= int(input("which accent do you want? \n 1) Australian \n 2) American \n 3) British \n 4)Indian \n 5) Canadian \n 6) Irish \n 7) South African \n 8) Spanish\n\n(Select the number corresponding): "))
 
 if b == 1:
     a = 'com.au'
@@ -22,6 +22,9 @@ elif b == 6:
     a = 'ie'
 elif b == 7:
     a = 'co.za'
+elif b == 8:
+    a = 'es'
+
 obj= gTTS(text=text, lang=language, slow=False, tld= a )
 Filename= input("Enter Audio Filename to Save (With Extension): ")
 obj.save(Filename)
